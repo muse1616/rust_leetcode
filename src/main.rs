491d7b675e8770f924e53579ed1fbf7d7ea5e309
jiajun.hu <jiajun.hu@shopee.com>
2023-05-11 17:06:05 +0800

    update

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main(){
+}
+struct Solution{}
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        use std::collections::HashMap;
+        let mut m = HashMap::new();
+        for (index,value) in nums.iter().enumerate() {
+            if m.contains_key(&(target-value)){
+                return vec![index as i32,m[&(target-value)]];
+            }
+            m.insert(value, index as i32);
+        }
+        return vec![];
+    }
+}
